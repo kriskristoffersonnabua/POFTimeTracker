218068055af24d402508a801a0175357cb4c72d5
Devs Devaras <devsdevaras@Admins-Mac-mini.lan>
2020-05-04 22:43:23 +0800

    Added API for Activity Files

---
+++ app/Http/Controllers/API/Activities/ActivityFilesController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api\Activities;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+class ActivityFilesController extends ActivityFilesController
+{
+    
+}
---
+++ app/Models/Activities/ActivityFile.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models\Activities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AcitivityFile extends Model
+{
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'activity_files';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+}
