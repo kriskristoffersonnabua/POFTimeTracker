ae68f97326836c29524bd23073d2cd5b527d051b
jareinnejae <jareinne.lagarto@goabroad.com>
2020-05-14 20:59:39 +0800

    Add activities attrib to SubProjects and subprojects attrib to Projects

--- app/Models/Activity/Activity.php
+++ app/Models/Activity/Activity.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Activity extends Model
 {
     protected $table = 'activities';
+    protected $primaryKey = 'id';
     protected $fillable = [
         'subproject_id',
         'activity_no',
